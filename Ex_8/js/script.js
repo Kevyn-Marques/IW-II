7a6a548007458b834df654b07c5c8b0454b72906
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-03-20 17:52:52 -0300

    Update script.js

--- Ex_8/js/script.js
+++ Ex_8/js/script.js
@@ -1,15 +1,15 @@
 const bt = document.getElementById("calc");
-const calcular = document.getElementById("calcular");
-const resultados = document.getElementById("resultados");
+const p = document.getElementById("p");
+const r = document.getElementById("r");
 
-function calcu(){
+function calc(){
     var a = prompt("Digite o valor de a:");
     var b = prompt("Digite o valor de b:");
     var c = prompt("Digite o valor de c:");
 
     var del = b*b - 4*a*c;
-    calcular.innerHTML = "O cálculo realizado será &Delta; = " + b + "<sup>2</sup> - " + "4." + a + "." + c
-    resultados.innerHTML = "O valor calculado foi &Delta; = " + del;
+    p.innerHTML = "O cálculo realizado será &Delta; = " + b + "<sup>2</sup> - " + "4." + a + "." + c
+    r.innerHTML = "O valor calculado foi &Delta; = " + del;
 }
 
-bt.addEventListener("click", calcu);
+bt.addEventListener("click", calc);
