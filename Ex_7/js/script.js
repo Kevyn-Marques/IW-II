1bd29012c4eb9924c869c5a712871cc7bfc24294
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-03-20 17:41:35 -0300

    Update script.js

--- Ex_7/js/script.js
+++ Ex_7/js/script.js
@@ -2,13 +2,11 @@ const salar = document.getElementById("salar");
 const resul = document.getElementById("resul");
 const finalz = document.getElementById("finalz");
 var nome = ' '
-var sal = 0
-var aumenta = 0
 
 function reajustar(){
     nome = prompt("Digite o nome do funcionário:");
-    sal = prompt("Digite o salário atual de " + nome + ":");
-     aumeta = prompt("Digite o reajuste do salário em porcentagem: ");
+    var sal = prompt("Digite o salário atual de " + nome + ":");
+    var aumeta = prompt("Digite o reajuste do salário em porcentagem: ");
 
     sal = Number(sal);
     aumenta = Number(aumenta);
