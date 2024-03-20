50496bc3123c14ff1403f065d19428b6476beebf
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-03-20 17:39:43 -0300

    Update script.js

--- Ex_8/js/script.js
+++ Ex_8/js/script.js
@@ -1,9 +1,15 @@
-const comecar = document.getElementById("comecar")
-var nome = ' '
+const bt = document.getElementById("calc");
+const calcular = document.getElementById("calcular");
+const resultados = document.getElementById("resultados");
 
-function interagi(){
-    nome = prompt("Qual seu nome?")
-    alert("Olá " + nome + " beleza?" ) 
+function calcu(){
+    var a = prompt("Digite o valor de a:");
+    var b = prompt("Digite o valor de b:");
+    var c = prompt("Digite o valor de c:");
+
+    var del = b*b - 4*a*c;
+    calcular.innerHTML = "O cálculo realizado será &Delta; = " + b + "<sup>2</sup> - " + "4." + a + "." + c
+    resultados.innerHTML = "O valor calculado foi &Delta; = " + del;
 }
 
-comecar.addEventListener('click',interagi)
+bt.addEventListener("click", calcu);
