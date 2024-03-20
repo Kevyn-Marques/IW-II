ca0ba50d0611b83de2b0bb14c95a17d42dd64de4
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-03-20 17:25:00 -0300

    Update script.js

--- Ex_7/js/script.js
+++ Ex_7/js/script.js
@@ -1,9 +1,17 @@
-const comecar = document.getElementById("comecar")
-var nome = ' '
+const button = document.getElementById("button");
+const p = document.getElementById("p");
+const r = document.getElementById("r");
 
-function interagi(){
-    nome = prompt("Qual seu nome?")
-    alert("Olá " + nome + " beleza?" ) 
+function reajustar(){
+    var nome = prompt("Digite o nome do funcionário:");
+    var sal = prompt("Digite o salário atual de " + nome + ":");
+    var aum = prompt("Digite o reajuste do salário em porcentagem: ");
+
+    sal = Number(sal);
+    aum = Number(aum);
+    var porc = (sal/100)*aum;
+    p.innerHTML = "O salário de " + nome + " era de R$" + sal + ",00. Acrescentando " + aum + "%, O salário irá aumentar R$"+ porc + ",00";
+    r.innerHTML = "Agora seu salário será de R$" + (sal+porc) + ",00"
 }
 
-comecar.addEventListener('click',interagi)
+button.addEventListener("click", reajustar)
