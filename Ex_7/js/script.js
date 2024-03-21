24d04bb209a1e99cfc329c0f6cd677f04bfcbe1b
Kevyn-Marques <takinhoverycrazy@gmail.com>
2024-03-21 07:49:40 -0300

    acabado mlk

--- Ex_7/js/script.js
+++ Ex_7/js/script.js
@@ -5,17 +5,17 @@ var nome = ' '
 
 function reajustar(){
     nome = prompt("Digite o nome do funcionário:");
-    var sal = prompt("Digite o salário atual de " + nome + ":");
-    var aumeta = prompt("Digite o reajuste do salário em porcentagem: ");
+    var aumentar = prompt("Digite o salário atual de " + nome + ":");
+    var sal = prompt("Digite o reajuste do salário em porcentagem: ");
 
     sal = Number(sal);
-    aumenta = Number(aumenta);
+    aumentar = Number(aumentar);
     
     
-    var porcen = (sal/100)*aumenta;
+    var porcen = (sal/100)*aumentar;
     
-    resul.innerHTML = "O salário de " + nome + " era de R$" + sal + ",00. Acrescentando " + aumenta + "%, O salário irá aumentar R$"+ porcen + ",00";
-    finalz.innerHTML = "Agora seu salário será de R$" + (sal+porcen) + ",00"
+    resul.innerHTML = "O salário de " + nome + " era de R$" + aumentar + ",00. Acrescentando " + sal + "%, O salário irá aumentar R$"+ porcen + ",00";
+    finalz.innerHTML = "Agora seu salário será de R$" + (aumentar+porcen) + ",00"
 }
 
 salar.addEventListener("click", reajustar)
