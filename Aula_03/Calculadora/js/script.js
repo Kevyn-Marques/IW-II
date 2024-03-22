26f1d0fb6143884d583691a7ae8f6ec837ad3538
Kevyn-Marques <takinhoverycrazy.com>
2024-03-22 11:51:46 -0300

    esqueleto

--- Aula_03/Calculadora/js/script.js
+++ Aula_03/Calculadora/js/script.js
@@ -7,6 +7,7 @@ const mult = document.getElementById('mult')
 const divi = document.getElementById('divi')
 const resultado = document.getElementById('resultado')
 const dansa = document.getElementById('dansa')
+const limpar = document.getElementById('limpa')
 var num1 = 0
 var num2 = 0
 var conta = 0
@@ -40,6 +41,12 @@ function Dividir(){
     resultado.innerHTML = (conta) 
     dansa.src = "img/esquelis.gif"  
 }
+function limpa(){
+    viraNum1.value = ""
+    viraNum2.value  = ""
+    dansa.src= ""
+    resultado.innerHTML = ""
+}
 
 //----------------Eventos-------------------
 
@@ -47,3 +54,4 @@ somar.addEventListener('click',Mais)
 sub.addEventListener('click',Menos)
 mult.addEventListener('click',Vezes)
 divi.addEventListener('click',Dividir)
+limpar.addEventListener('click',limpa)
