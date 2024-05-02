e87add616f471522ed5bce1bb3112b40314023bd
Kevyn-Marques <takinhoverycrazy@gmail.com>
2024-05-02 11:52:08 -0300

    alterações

--- Jogo Mauro/js/script.js
+++ Jogo Mauro/js/script.js
@@ -1,4 +1,5 @@
 const benio = document.querySelector(".benio")
+const pipe = document.querySelector(".pipe")
 
 const jump = ()=>{
     benio.classList.add("pulo")
@@ -8,13 +9,37 @@ const jump = ()=>{
 
     },500)
 }
-const loop = setInterval(()=>{
-    const pipePosition = pipe.offsetLeft
-    
-    if(pipePosition <= 120){
-        pipe.style.animation = "none"
-        pipe.style.left = `$(pipePosition)px`
+const loopGame = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const benPosition = +window.getComputedStyle(benio).bottom.replace('px', ''); 
+
+    if(pipePosition <= 120 && pipePosition > 0 && benPosition < 80 ){ 
+        pipe.style.animation = 'none'; 
+        pipe.style.left = `${pipePosition}px`; 
+
+        benio.style.animation = 'none'; 
+        benio.style.bottom = `${benPosition+25}px`; 
+
+        benio.src = './img/morto.gif';
+        benio.style.width = '150px';
+        benio.style.marginLeft = '0';
+
+        clearInterval(loopGame);
+        clearInterval(pontuacao)
     }
+
 }, 10)
+
+function reiniciar(){
+    window.location.reload()
+}
+function tempo(){
+    var score = document.getElementById("score");
+    pontos = parseInt(score.textContent);
+    pontos++;
+    score.textContent = pontos;
+}
+const pontuacao = setInterval(tempo,100)
 
 document.addEventListener('keydown',jump)
